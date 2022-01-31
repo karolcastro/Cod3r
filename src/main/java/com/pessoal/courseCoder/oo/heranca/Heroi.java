63c59435c49f5ecd774c9e420aaabd5ee1428a34
karol castro <karolinycosta@yahoo.com.br>
2022-01-31 18:58:21 -0300

    estabelecendo a sobreescrita de codigo

--- src/main/java/com/pessoal/courseCoder/oo/heranca/Heroi.java
+++ src/main/java/com/pessoal/courseCoder/oo/heranca/Heroi.java
@@ -2,4 +2,20 @@ package com.pessoal.courseCoder.oo.heranca;
 
 public class Heroi extends Jogador {
 
+
+    boolean atacar(Jogador oponente){
+        int deltaX = Math.abs(x - oponente.x);
+        int destaY = Math.abs(y - oponente.y);
+
+        if(deltaX == 0 && destaY == 1) {
+            oponente.vida -= 20;
+            return true;
+        } else if(deltaX == 1 && destaY == 0) {
+            oponente.vida -= 20;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
