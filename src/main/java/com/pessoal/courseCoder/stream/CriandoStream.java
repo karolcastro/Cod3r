9fc34628e502c6f6ee2da19a883a4f81c07e2afd
karol castro <karolinycosta@yahoo.com.br>
2022-06-01 17:22:33 -0300

    Stream

---
+++ src/main/java/com/pessoal/courseCoder/stream/CriandoStream.java
@@ -0,0 +1,13 @@
+package com.pessoal.courseCoder.stream;
+
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+
+public class CriandoStream {
+    public static void main(String[] args) {
+
+        Consumer<String> print = System.out::print;
+
+        Stream<String> langs =
+    }
+}
